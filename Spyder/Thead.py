68ed8701cfc14c3b2ec44ecd71380b8f95c607f4
jiangzeyu5 <Noir-desire>
2018-03-21 14:46:00 +0800

    多线程爬虫案例，添加注释
    Signed-off-by: jiangzeyu5 <Noir-desire>

--- Spyder/Thead.py
+++ Spyder/Thead.py
@@ -8,12 +8,13 @@
 import requests
 from lxml import etree
 from time import time
-from threading import Thread
+from threading import Thread                                 #多线程
 
-url = 'http://movie.douban.com/top250'
+url = 'https://movie.douban.com/top250'
 
 
 def parse(url):
+    '''多线程案例'''
     response = requests.get(url)
     page = response.content
     html = etree.HTML(page)
@@ -25,10 +26,10 @@ def parse(url):
     pages = html.xpath(xpath_pages)
     fetch_list = []
     result = []
-
+    '''解析首页，将内容返回result'''
     for element_movie in html.xpath(xpath_movie):
         result.append(element_movie)
-
+    '''解析页码，拼接url'''
     for p in pages:
         fetch_list.append(url + p.get('href'))
 
@@ -39,28 +40,25 @@ def parse(url):
 
         for element_movie in html.xpath(xpath_movie):
             result.append(element_movie)
-
+    '''线程设置启动'''
     threads = []
     for url in fetch_list:
         t = Thread(target=fetch_content, args=[url])
         t.start()
         threads.append(t)
-
     for t in threads:
         t.join()
 
-    for i,movie in enumerate(result,1):
+    for i, movie in enumerate(result,1):
         title = movie.find(xpath_title).text
-
+        print(i,title)
 
 
 def main():
-    from time import time
     start = time()
-    for i in range(5):
-        parse(url)
+    parse(url)
     end = time()
-    print ('Cost {} seconds'.format((end - start) / 5))
+    print ('Cost {} seconds'.format(end - start))
 
 if __name__ == '__main__':
-    parse(url)
+    main()
