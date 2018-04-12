1c93560f527f6ed1ab69257609fcb915fcce5656
jiangzeyu5 <Noir-desire>
2018-04-12 17:49:22 +0800

    51爬取完成
    Signed-off-by: jiangzeyu5 <Noir-desire>

--- employ/SpyRecruit/crawlend/spiders/job51.py
+++ employ/SpyRecruit/crawlend/spiders/job51.py
@@ -106,4 +106,101 @@ class Job51Spider(scrapy.Spider):
         # 公司名
         firm['firm_name'] = soup_cn.find('p', class_='cname').get_text(strip=True)
         # 行业规模
-        soup_firm_msg = soup_cn.find('p', class_='msg ltype').
+        soup_firm_msg = soup_cn.find('p', class_='msg ltype')
+        str_msg = soup_firm_msg.string.replace('&nbsp;', '')
+        nature, scale, indus = [i.strip() for i in str_msg.split('|')]
+
+        # 企业性质
+        # 编码规范化入库
+        if '外企' in nature:
+            firm['firm_nature'] = '2'
+        elif '合资' in nature:
+            firm['firm_nature'] = '3'
+        elif '国企' in nature:
+            firm['firm_nature'] = '1'
+        elif '民营' in nature:
+            firm['firm_nature'] = '4'
+        elif '上市' in nature:
+            firm['firm_nature'] = '7'
+        else:
+            firm['firm_nature'] = '5'
+
+        # 企业规模
+        p_scale = re.compile(r'\d+')
+        lst_scale = re.findall(p_scale, scale)
+        lst_scale = [int(i) for i in lst_scale]
+        lst_scale.sort()
+        if len(lst_scale) == 2:
+            firm['firm_scale_from'], firm['firm_scale_to'] = lst_scale
+        elif len(lst_scale) == 1:
+            firm['firm_scale_from'] = lst_scale[0]
+
+        # 行业
+        firm['firm_industry'] = indus
+
+        # 职位要求
+        soup_job_qua = soup.find('div', class_= 'jtag inbox')
+        soup_qua_div = soup_job_qua.find('div', class_='t1')
+        soup_qua_span = soup_qua_div.find_all('span')
+        # 正则式匹配经验年限
+        p_exp = re.compile(r'\d+')
+        # 正则式匹配发布时间
+        p_date = re.compile(r'\d+-\d+')
+        today = datetime.date.today()
+        year_ = today.year
+        for span in soup_qua_span:
+            text = span.get_text(strip=True)
+            # 处理经验
+            if '经验' in text:
+                r_exp = re.findall(p_exp, text)
+                r_exp.sort()  # 排序
+                if len(r_exp) == 2:
+                    offer['years_of_work_from'], offer['years_of_work_to'] = r_exp
+                elif len(r_exp) == 1:
+                    offer['years_of_work_from'] = r_exp[0]
+            # 学历
+            if '高中' in text:
+                offer['degree'] = '1'
+            elif '大专' in text:
+                offer['degree'] = '2'
+            elif '本科' in text:
+                offer['degree'] = '3'
+            elif '研究生' in text:
+                offer['degree'] = '4'
+            elif '博士' in text:
+                offer['degree'] = '5'
+            # 招聘人数
+            if '招聘':
+                r_mem = re.findall(p_exp, text)
+                if len(r_mem) == 1:
+                    offer['member'] = r_mem[0]
+            # 发布时间
+            if '发布':
+                r_date = re.findall(p_date, text)
+                if r_date:
+                    date_ = str(year_) + '-' + r_date[0]
+                    offer['release'] = datetime.datetime.strptime(date_, "%Y-%m-%d").date()
+
+        offer.setdefault('degree', '6')
+        offer.setdefault('release', today)
+        # 职位诱惑
+        soup_r = soup_job_qua.find('p', class_='t2')
+        if soup_r:
+            offer['temptation'] = soup_r.get_text(';', strip=True)
+        soup_qua = soup.find('div', class_='bmsg job_msg inbox')
+        # 职位描述
+        offer['description'] = soup_qua.get_text(strip=True).replace('举报', '').replace('分享', '')
+
+        # 公司地址
+        soup_loc_div = soup.find('div', class_='bmsg inbox')
+        soup_loc_p = soup_loc_div.find('p', class_='fp')
+        firm['firm_location'] = [i for i in soup_loc_p.stripped_strings][-1]
+
+        # 公司简介
+        soup_intro = soup.find('div', class_='tmsg inbox')
+        firm['firm_introduction'] = soup_intro.get_text(strip=True)
+
+        item['offer'] = offer
+        item['firm'] = firm
+        yield item
+
